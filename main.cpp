a86f6a800e93dfec86649d449fe43d95ed471792
Disum <54613404+Disum@users.noreply.github.com>
2019-08-31 15:10:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@ public:
 	// Конструктор
 	Matrix(int m, int n) :elem{new T[m*n]}, numRows{m}, numColumns{n} { }
 
+	// Конструктор с копированием
+	Matrix(const Matrix<T>& other) :elem{new T[other.numRows*other.numColumns]}, numRows{other.numRows}, numColumns{other.numColumns} {
+		for (int i = 0; i!=numRows; ++i)
+			for (int j = 0; j!=numColumns; ++j)
+				elem[i*numColumns + j] = other(i,j);
+
+	}
+
 	// Доступ к элементам по индексам
 	T& operator()(int m, int n) { return elem[m*numColumns + n]; }
 
@@ -40,6 +48,7 @@ public:
 		return *this;
 	}
 
+	// Считывание
 	void scan() {
 		for (int i = 0; i!=numRows; ++i)
 			for (int j = 0; j!=numColumns; ++j) {
@@ -48,6 +57,7 @@ public:
 			}
 	}
 
+	// Вывод
 	void print() {
 		for (int i = 0; i!=numRows; ++i) {
 			for (int j = 0; j!=numColumns; ++j)
@@ -70,25 +80,36 @@ int main() {
 	cout << "Число столбцов матрицы:\t";
 	cin >> numColumns;
 	Matrix<int> iMatr(numRows, numColumns);
-	//iMatr.scan();
-	//iMatr.print();
-	
-	iMatr = iMatr;
-	
+	iMatr.scan();
+	iMatr.print();
+
+	/*
+	cout << "iMatr(0,0)=" << iMatr(0,0) << endl;
+	iMatr(0,0) = iMatr(0,1);
+	cout << "iMatr(0,0)=" << iMatr(0,0) << endl;
+	*/
+
+	//iMatr = iMatr;
+
+	/*
 	cout << "iMatr2:" << endl << "Число строк матрицы:\t";
 	cin >> numRows;
 	cout << "Число столбцов матрицы:\t";
 	cin >> numColumns;
-	Matrix<int> iMatr2(numRows, numColumns);
+	*/
+	Matrix<int> iMatr2 = iMatr;
 	//iMatr2.scan();
-	//iMatr2.print();
+	iMatr2.print();
 
+	/*
 	cout << "iMatr3:" << endl << "Число строк матрицы:\t";
 	cin >> numRows;
 	cout << "Число столбцов матрицы:\t";
 	cin >> numColumns;
 	Matrix<int> iMatr3(numRows, numColumns);
+	*/
 
+	/*
 	try { iMatr3 = iMatr2 + iMatr; }
 	catch (invalid_argument e) {
 		cout << "Error!" << endl;
@@ -96,6 +117,7 @@ int main() {
 
 		return 0;
 	}
+	*/
 
 	//cout << "iMatr3:" << endl;
 	//iMatr3.print();
